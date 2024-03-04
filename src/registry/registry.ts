417713e6c58492b8e08e6919abf099379de07a89
LUSARDI <enzo.lusardi@edu.devinci.fr>
2024-03-04 16:07:46 +0100

    1

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -1,8 +1,11 @@
-import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 import { REGISTRY_PORT } from "../config";
 
-export type Node = { nodeId: number; pubKey: string };
+export type Node = {
+  nodeId: number;
+  pubKey: string;
+};
 
 export type RegisterNodeBody = {
   nodeId: number;
@@ -14,15 +17,33 @@ export type GetNodeRegistryBody = {
 };
 
 export async function launchRegistry() {
-  const _registry = express();
-  _registry.use(express.json());
-  _registry.use(bodyParser.json());
+  const app = express();
+  app.use(express.json());
+  app.use(bodyParser.json());
 
-  // TODO implement the status route
-  // _registry.get("/status", (req, res) => {});
+  // On définit la route pour le statut du Registre
+  app.get("/status", (req, res) => {
+    res.send("live");
+  });
 
-  const server = _registry.listen(REGISTRY_PORT, () => {
-    console.log(`registry is listening on port ${REGISTRY_PORT}`);
+  // On initialise une variable qui coontiendra tous les noeuds : le registre
+  let nodesRegistry: Node[] = [];
+
+  // On définis la route pour enregistrer un noeud
+  app.post("/registerNode", (req: Request, res: Response) => {
+    const { nodeId, pubKey }: { nodeId: number; pubKey: string } = req.body;
+    nodesRegistry.push({ nodeId, pubKey });
+    res.status(200).send("Node registered successfully");
+  });
+
+  // Ici on définit la route pour obtenir le registre des noeuds
+  app.get("/getNodeRegistry", (req, res) => {
+    res.json({ nodes: nodesRegistry });
+  });
+
+  // Finalement on peut démarer le serveur Registry
+  const server = app.listen(REGISTRY_PORT, () => {
+    console.log(`Registry is listening on port ${REGISTRY_PORT}`);
   });
 
   return server;
